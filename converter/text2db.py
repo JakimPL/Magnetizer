8c17d11591ccf4aee066077e183748063dc3291d
JakimPL <jakimpl@gmail.com>
2023-04-21 20:55:57 +0200

    Updated: text converter

--- converter/text2db.py
+++ converter/text2db.py
@@ -2,12 +2,20 @@ import argparse
 
 from auxiliary import to_hex
 
+SPECIAL_CHARACTERS = {
+    '-': 40,
+    '/': 41,
+    ':': 42,
+    '!': 43,
+    ' ': 44
+}
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('text', help='text to convert')
     args = parser.parse_args()
 
-    text = [letter for letter in args.text.upper() if letter.isalnum() or letter in [' ', ':', '/']]
+    text = [letter for letter in args.text.upper() if letter.isalnum() or letter in SPECIAL_CHARACTERS.keys()]
     text_string = ''.join(text).lower().replace(' ', '_').replace(':', '').replace('/', '')
 
     digit_char_start = ord('0')
@@ -15,12 +23,8 @@ if __name__ == '__main__':
 
     values = [len(text)]
     for char in text:
-        if char == ':':
-            value = 42
-        elif char == '/':
-            value = 43
-        elif char == ' ':
-            value = 44
+        if char in SPECIAL_CHARACTERS:
+            value = SPECIAL_CHARACTERS[char]
         elif char.isdigit():
             value = ord(char) - digit_char_start
         else:
